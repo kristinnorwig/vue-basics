05ee177586ca0b24e4bdaec48d852437d46a3ac8
Kristin Norwig <kristin.norwig@gmail.com>
2023-12-01 09:31:40 +0100

    feat: adds comments

--- 02_from_HTML_to_Vue/script.js
+++ 02_from_HTML_to_Vue/script.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
   data() {
     return {
       name: "John Doe",
-      currDateAndTime: new Date().toLocaleString("en-GB"),
+      currDateAndTime: new Date().toLocaleString("en-GB"), // kann auch direkt in Mustache Tags in HTML gegeben werden
     };
   },
 }).mount("#card");
+
+// In JavaScript wird das Schlüsselwort new verwendet,
+// um eine Instanz eines Objekts zu erstellen.
+// => neue Instanz des "Date"-Objekts wird erstellt
