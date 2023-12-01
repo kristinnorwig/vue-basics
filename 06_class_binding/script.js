2025905eecdf21d7a2f286d7da2b6ab7139ee120
Kristin Norwig <kristin.norwig@gmail.com>
2023-12-01 10:16:25 +0100

    feat: adds class binding task solution

---
+++ 06_class_binding/script.js
@@ -0,0 +1,19 @@
+Vue.createApp({
+  data() {
+    return {
+      activeLetter: "A",
+    };
+  },
+}).mount("#app");
+
+/*methods: {
+    renderCurrBtn(activeLetter) {
+      if (activeLetter === "A") {
+        return "btn-active";
+      } else if (activeLetter === "B") {
+        return "btn-active";
+      } else if (activeLetter === "C") {
+        return "btn-active";
+      }
+    },
+  },*/
