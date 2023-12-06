edf21c2bfae9a10493ae81f8e7dbf834f53253e9
Kristin Norwig <kristin.norwig@gmail.com>
2023-12-06 16:56:29 +0100

    feat: adds random color with vue

--- 15_color_mixer_with_vuejs/script.js
+++ 15_color_mixer_with_vuejs/script.js
@@ -1,9 +1,9 @@
 Vue.createApp({
   data() {
     return {
-      red: 30,
-      green: 144,
-      blue: 255,
+      red: 5,
+      green: 5,
+      blue: 5,
     };
   },
   methods: {
@@ -16,23 +16,6 @@ Vue.createApp({
     getRandomHexValue() {
       return Math.floor(Math.random() * 256); // Zufällige Zahl zwischen 0 und 255
     },
-
-    /*changeBgColor() {
-      const redInput = document.getElementById("red").value;
-      const blueInput = document.getElementById("blue").value;
-      const greenInput = document.getElementById("green").value;
-      bodyEl.style.backgroundColor =
-        "rgb(" + redInput + ", " + greenInput + ", " + blueInput + ")"; //setzen des styles auf den Background
-      valueHex(redInput, blueInput, greenInput);
-    },
-
-    valueHex(r, g, b) {
-      r = Number(r).toString(16); // 16 bedeutet hier dass Zahl in Hexadezimalsystem umgewandelt wird (Zeichen 0-9 und den Buchstaben A-F)
-      g = Number(g).toString(16);
-      b = Number(b).toString(16);
-
-      hexPEl.innerText = convertToHex(r) + convertToHex(g) + convertToHex(b);
-    }, */
 
     convertToHex(value) {
       if (value < 16) {
@@ -52,3 +35,22 @@ Vue.createApp({
     },
   },
 }).mount("#app");
+
+/*Fehlt noch: 
+ 
+ changeBgColor() {
+      const redInput = document.getElementById("red").value;
+      const blueInput = document.getElementById("blue").value;
+      const greenInput = document.getElementById("green").value;
+      bodyEl.style.backgroundColor =
+        "rgb(" + redInput + ", " + greenInput + ", " + blueInput + ")"; //setzen des styles auf den Background
+      valueHex(redInput, blueInput, greenInput);
+    },
+
+    valueHex(r, g, b) {
+      r = Number(r).toString(16); // 16 bedeutet hier dass Zahl in Hexadezimalsystem umgewandelt wird (Zeichen 0-9 und den Buchstaben A-F)
+      g = Number(g).toString(16);
+      b = Number(b).toString(16);
+
+      hexPEl.innerText = convertToHex(r) + convertToHex(g) + convertToHex(b);
+    }, */
