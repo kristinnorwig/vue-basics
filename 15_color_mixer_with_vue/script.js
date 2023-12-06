ea7bf65e16a3e906c7d320e44ea8b634e3b2fffc
Kristin Norwig <kristin.norwig@gmail.com>
2023-12-06 16:58:38 +0100

    fix: correcting name

---
+++ 15_color_mixer_with_vue/script.js
@@ -0,0 +1,56 @@
+Vue.createApp({
+  data() {
+    return {
+      red: 5,
+      green: 5,
+      blue: 5,
+    };
+  },
+  methods: {
+    giveRndmColor() {
+      this.red = this.getRandomHexValue();
+      this.green = this.getRandomHexValue();
+      this.blue = this.getRandomHexValue();
+    },
+
+    getRandomHexValue() {
+      return Math.floor(Math.random() * 256); // Zufällige Zahl zwischen 0 und 255
+    },
+
+    convertToHex(value) {
+      if (value < 16) {
+        return "0" + value.toString(16);
+      } else {
+        return value.toString(16);
+      }
+    },
+  },
+  computed: {
+    hexCode() {
+      return (
+        this.convertToHex(this.red) +
+        this.convertToHex(this.green) +
+        this.convertToHex(this.blue)
+      );
+    },
+  },
+}).mount("#app");
+
+/*Fehlt noch: 
+ 
+ changeBgColor() {
+      const redInput = document.getElementById("red").value;
+      const blueInput = document.getElementById("blue").value;
+      const greenInput = document.getElementById("green").value;
+      bodyEl.style.backgroundColor =
+        "rgb(" + redInput + ", " + greenInput + ", " + blueInput + ")"; //setzen des styles auf den Background
+      valueHex(redInput, blueInput, greenInput);
+    },
+
+    valueHex(r, g, b) {
+      r = Number(r).toString(16); // 16 bedeutet hier dass Zahl in Hexadezimalsystem umgewandelt wird (Zeichen 0-9 und den Buchstaben A-F)
+      g = Number(g).toString(16);
+      b = Number(b).toString(16);
+
+      hexPEl.innerText = convertToHex(r) + convertToHex(g) + convertToHex(b);
+    }, */
